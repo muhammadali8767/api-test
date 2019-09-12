5f8da5c2b06ac785eaa94b11effe3899bc8f8205
= <=>
2019-09-12 14:57:55 +0500

    last changes 2

--- server.php
+++ server.php
@@ -1,48 +1,51 @@
 <?php
 
-        if (isset($_POST['name'])) {
-            $name = $_POST['name'];
-            $email = $_POST['email'];
+    if (isset($_POST['name'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
 
-            if( $name == 'Davlatyor' && $email == 'davlatyor@mail.com'){
-                $return = [
-                    'method' => "POST",
-                    'success' => true,
-                    'message' => "You are logged in!"
-                ];
-            } else {
-                $return = [
-                    'method' => "POST",
-                    'success' => false,
-                    'message' => "You are not logged in!"
-                ];
-            }
-            header('Content-type: application/json');
-            echo json_encode($return);
-
+        if( $name == 'Davlatyor' && $email == 'davlatyor@mail.com'){
+            $return = [
+                'method' => "POST",
+                'success' => true,
+                'message' => "You are logged in!"
+            ];
+        } else {
+            $return = [
+                'method' => "POST",
+                'success' => false,
+                'message' => "You are not logged in!"
+            ];
         }
 
-        if (isset($_GET['name'])) {
-            $name = $_GET['name'];
-            $email = $_GET['email'];
-    
-            if( $name == 'Davlatyor' && $email == 'davlatyor@mail.com'){
-                $return = [
-                    'method' => "GET",
-                    'success' => true,
-                    'message' => "You are logged in!"
-                ];
-            } else {
-                $return = [
-                    'method' => "GET",
-                    'success' => false,
-                    'message' => "You are not logged in!"
-                ];
-            }
-    
-            header('Content-type: application/json');
-            echo json_encode($return);
+        header('Content-type: application/json');
+        echo json_encode($return);
+    }
 
+    if (isset($_GET['name'])) {
+        $name = $_GET['name'];
+        $email = $_GET['email'];
+
+        if( $name == 'Davlatyor' && $email == 'davlatyor@mail.com'){
+            $return = [
+                'method' => "GET",
+                'success' => true,
+                'message' => "You are logged in!"
+            ];
+        } else {
+            $return = [
+                'method' => "GET",
+                'success' => false,
+                'message' => "You are not logged in!"
+            ];
         }
 
-?>
+        header('Content-type: application/json');
+        echo json_encode($return);
+    }
+
+    header('Content-type: application/json');
+    echo json_encode([
+        'success' => false,
+        'message' => "Invalid request!"
+    ]);
