f13d27595d631b0f6d275c6c826838652eb84b8c
muhammadali8767 <muhammadali8767@gmail.com>
2019-09-12 14:50:39 +0500

    last changes

---
+++ server.php
@@ -0,0 +1,48 @@
+<?php
+
+        if (isset($_POST['name'])) {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+
+            if( $name == 'Davlatyor' && $email == 'davlatyor@mail.com'){
+                $return = [
+                    'method' => "POST",
+                    'success' => true,
+                    'message' => "You are logged in!"
+                ];
+            } else {
+                $return = [
+                    'method' => "POST",
+                    'success' => false,
+                    'message' => "You are not logged in!"
+                ];
+            }
+            header('Content-type: application/json');
+            echo json_encode($return);
+
+        }
+
+        if (isset($_GET['name'])) {
+            $name = $_GET['name'];
+            $email = $_GET['email'];
+    
+            if( $name == 'Davlatyor' && $email == 'davlatyor@mail.com'){
+                $return = [
+                    'method' => "GET",
+                    'success' => true,
+                    'message' => "You are logged in!"
+                ];
+            } else {
+                $return = [
+                    'method' => "GET",
+                    'success' => false,
+                    'message' => "You are not logged in!"
+                ];
+            }
+    
+            header('Content-type: application/json');
+            echo json_encode($return);
+
+        }
+
+?>
